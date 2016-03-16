78208778a4ad29453d24ab9b7117ebc6aa7c061d
daniel-llach <daniel.llach@gmail.com>
2016-03-16 09:46:39 -0300

    basic list

--- component/dw-list.js
+++ component/dw-list.js
@@ -2,6 +2,17 @@ var scripts = document.getElementsByTagName("script");
 var urlBase = scripts[scripts.length-1].src;
 urlBase = urlBase.replace('dw-list.js', '');
 
+let $from;
+let $to;
+
+let $fromItem;
+let $toItem;
+
+let $indicator;
+
+let $fromItemSelector;
+let $toItemSelector;
+
 // dwFilter
 (function( $ ){
   "use strict"
@@ -30,10 +41,8 @@ urlBase = urlBase.replace('dw-list.js', '');
     },
     restart: function($el){
       // previene cuando no hay input
-      let $groups = $el.find('.items .group');
-      let $groupsContent = $el.find('.items .group-content');
-      $groups.show();
-      $groupsContent.show();
+      let $content = $el.find('.items .group-content');
+      $content.show();
 
       // previene cuando no hay input
       let $items = $el.find('.items .option');
@@ -66,9 +75,7 @@ urlBase = urlBase.replace('dw-list.js', '');
     setTemplate : function($el, templateContent, options){
 
       let template = _.template(templateContent);
-      $el.html( template({
-        'placeholder': options.placeholder
-      }) );
+      $el.html( template() );
 
       if (typeof options !== 'undefined') {
         methods.itemTemplate($el, options)
@@ -76,141 +83,68 @@ urlBase = urlBase.replace('dw-list.js', '');
 
     },
     itemTemplate: function($el, options){
-
       let data = options.data[0];
 
-      // If has groups, paint groups containers
-      if( data.hasOwnProperty('group') ){
-        // define groups
-        let groups =  _.chain(options.data).flatten().pluck('group').flatten().unique().value().sort();
-
-        // paint groups containers
-        _.each(groups, function(group){
-          $el.find('content > .items').append('<div class="group" id="' + group + '"><div class="title"><span class="name">' + group + '</span><span class="open"></span></div></div><div class="group-content ' + group + '"></div>')
-        })
-
-        // put options into its group
-        $.get(urlBase + "templates/items.html", function( result ) {
-            let template = _.template(result);
-            let data = _.sortBy(options['data'], 'primary');
-
-            // options each
-            data.forEach(function (data, i) {
-              let contentHtml = template({
-                id: data['id'],
-                place: i + 1,
-                primary: data['primary'],
-                secundary: data['secundary']
-              });
-              // paint in specific group content
-              let group = data['group'];
-              $el.find('.' + group + '.group-content').append(contentHtml);
-            });
-
-            events.start($el, options);
-          });
-
-      }else{
-        // no groups
-        // put options into its group
-        $.get(urlBase + "templates/options.html", function( result ) {
-            let template = _.template(result);
-
-            let data = _.sortBy(options['data'], 'primary');
-
-            // options each
-            options['data'].forEach(data => {
-              let contentHtml = template({
-                id: data['id'],
-                primary: data['primary'],
-                secundary: data['secundary']
-              });
-              $el.find('content > .items').append(contentHtml);
-            });
-
-            events.start($el, options);
-          });
+      switch(options.type){
+        case 'change':
+          methods.changeTemplate($el, options);
+          break;
+        case 'order':
+          methods.orderTemplate($el, options);
+          break;
       }
-
     },
-    hideOptions: function($el, inputData, options){
+    orderTemplate: function($el, options){
+      // put items
+      $.get(urlBase + "templates/items.html", function( result ) {
+          let template = _.template(result);
+          // let data = options['data'];
+          let data = _.sortBy(options['data'], 'priority');
 
-      if( options.data[0].hasOwnProperty('group') ){
-
-        let firstLetter = inputData.charAt(0);
-        (firstLetter != ':') ? methods.hideOption($el, inputData, options) : methods.hideGroups($el, inputData, options);
-
-      }
-
-    },
-    hideOption: function($el, inputData, options){
-      let $option = $el.find('.option').toArray();
-
-      $option.forEach(opt => {
-
-        const $opt = $(opt);
-        let tempPrimary = $opt.find('.primary').text();
-        let tempSecundary = $opt.find('.secundary').text();
-
-        tempPrimary = tempPrimary.toLowerCase();
-        tempSecundary = tempSecundary.toLowerCase();
-
-        inputData = inputData.toLowerCase();
-
-        ( tempPrimary.indexOf(inputData) != -1 || tempSecundary.indexOf(inputData) != -1 ) ? $opt.show() : $opt.hide();
-
-      });
-
-    },
-    hideGroups: function($el, inputData, options){
-      let $groups = $el.find('.items .group').toArray();
-
-      $groups.forEach(grp => {
-        const $grp = $(grp);
-        let tempInput = $grp.find('.title .name').text()
-
-        if ( inputData.indexOf(' ') != -1 ){
-          let optTemp = inputData.split(' ');
-
-          // groups
-
-          optTemp[0] = optTemp[0].toLowerCase();
-          optTemp[0] = optTemp[0].replace(':','');
-          ( tempInput.indexOf(optTemp[0]) != -1 ) ? $grp.show() : $grp.hide();
-          ( tempInput.indexOf(optTemp[0]) != -1 ) ? $grp.next().show() : $grp.next().hide();
-
-
-
-          // options
-
-          let $option = $el.find('.option').toArray();
-
-          $option.forEach(opt => {
-
-            const $opt = $(opt);
-            let tempPrimary = $opt.find('.primary').text();
-            let tempSecundary = $opt.find('.secundary').text();
-
-            tempPrimary = tempPrimary.toLowerCase();
-            tempSecundary = tempSecundary.toLowerCase();
-
-            optTemp[1] = optTemp[1].toLowerCase();
-
-            ( tempPrimary.indexOf(optTemp[1]) != -1 || tempSecundary.indexOf(optTemp[1]) != -1 ) ? $opt.show() : $opt.hide();
-
+          // options each
+          data.forEach(function (data, i) {
+            let contentHtml = template({
+              id: data['id'],
+              priority: i + 1,
+              // priority: data['priority'],
+              primary: data['primary'],
+              secundary: data['secundary']
+            });
+            // paint it
+            $el.find('content .items').append(contentHtml);
           });
 
-        }else{
-
-          tempInput = tempInput.toLowerCase();
-          inputData = inputData.replace(':','');
-          inputData = inputData.toLowerCase();
-          ( tempInput.indexOf(inputData) != -1 ) ? $grp.show() : $grp.hide();
-          ( tempInput.indexOf(inputData) != -1 ) ? $grp.next().show() : $grp.next().hide();
-        }
-      });
-
+          // methods.order($el); // order
+          events.startOrder($el, options); // events
+        });
     },
+    changeTemplate: function($el, options){
+
+      // put items
+      $.get(urlBase + "templates/items.html", function( result ) {
+          let template = _.template(result);
+          // let data = options['data'];
+          let data = _.sortBy(options['data'], 'priority');
+
+          // options each
+          data.forEach(function (data, i) {
+            let contentHtml = template({
+              id: data['id'],
+              priority: i + 1,
+              // priority: data['priority'],
+              primary: data['primary'],
+              secundary: data['secundary']
+            });
+            // paint it
+            $el.find('content .items').append(contentHtml);
+          });
+
+          // methods.order($el); // order
+          events.startChange($el, options); // events
+        });
+
+      },
+
     getVal: function($el){
       // update $el data
       let items = $el.find('.items .option.selected').toArray();
@@ -233,6 +167,38 @@ urlBase = urlBase.replace('dw-list.js', '');
       $search.val(primaryContent);
       $search.focus();
 
+    },
+    updatePosition: function($el){
+      let $items = $el.find('.item').toArray();
+      $items.forEach(function(item, i){
+        $(item).find('.position').text(i+1);
+      });
+    },
+    swap: function($el, $from, $to, $fromSelector, $toSelector, options){
+      // console.log("$el: ", $el);
+      // console.log("$from: ", $from);
+      // console.log("$to: ", $to);
+      // console.log("$toSelector: ", $toSelector);
+      //
+
+        let $fromHtml = $fromSelector.html();
+        $el.find('.indicator').html($fromHtml).removeClass('indicator').attr('data-id', $from);
+
+        let $fromSelectorReview = $el.find('.items .item[data-id="' + $from + '"]');
+        console.log("$fromSelector: ", $fromSelector.length);
+
+        if($fromSelectorReview.length > 1){
+
+          $fromSelector.remove();
+
+
+          events.startOrder($el, options);
+          event.preventDefault();
+        }
+
+
+
+
     }
   }
 
@@ -240,20 +206,14 @@ urlBase = urlBase.replace('dw-list.js', '');
   // Events
   var events = {
 
-    start: function($el, options){
-      // events.onSearch($el, options);
-      // events.clearSearch($el, options);
-      // events.clickItems($el, options);
-      // events.clickOut($el, options);
-      events.dragItems($el, options);
+    startOrder: function($el, options){
+      events.dragItemsOrder($el, options);
+      methods.updatePosition($el);
+
     },
-    // initOptions: function($el, options){
-    //   let $option = $el.find('content > .options > .option');
-    //   $option.removeClass('hide');
-    //   $option.css({
-    //     'display': 'block'
-    //   })
-    // },
+    startChange: function($el, options){
+      events.dragItemsChange($el, options);
+    },
     toggleGroup: function($el, options){
     },
     clickItems: function($el, options){
@@ -271,7 +231,68 @@ urlBase = urlBase.replace('dw-list.js', '');
         }
       })
     },
-    dragItems: function($el, options){
+    dragItemsOrder: function($el, options){
+      let $items = $el.find('.items .item');
+
+
+
+      $items.bind({
+        dragstart: function(event){
+          $from = $(event.target).data('id');
+          $(event.target).addClass('fromMoved');
+
+          $fromItemSelector = $el.find('.items .item[data-id="' + $from + '"]');
+
+          let itemWidth = $fromItemSelector.outerWidth();
+
+          // $fromItemSelector.css({
+          //   'position': 'absolute',
+          //   'width': itemWidth,
+          //   'z-index': 99
+          // });
+        },
+        dragenter: function(event){
+          $to = $(event.target).data('id');
+
+          // console.log("to: ", $to);
+          $indicator = $el.find('.indicator');
+
+          $toItem = $el.find('.items .item[data-id="' + $to + '"]');
+          $indicator.remove();
+          $toItem.after('<li class="item indicator" draggable="true"></li>');
+          events.indicator($el, $indicator);
+
+        },
+        dragover: function(event){
+          if (event.preventDefault) {
+            event.preventDefault(); // Necessary. Allows us to drop.
+          }
+        },
+        dragend: function(event){
+          // console.log('dragend', $(event.target))
+          $(event.target).removeClass('fromMoved');
+
+          $fromItemSelector = $el.find('.items .item[data-id="' + $from + '"]');
+          $toItemSelector = $el.find('.items .item[data-id="' + $to + '"]');
+
+          methods.swap($el, $from, $to, $fromItemSelector, $toItemSelector, options);
+          // events.startOrder($el, options);
+
+        },
+        drop: function(event){
+
+        }
+      })
+
+    },
+    indicator: function($el, $indicator){
+      $indicator.bind({
+        startdrag: function(event){
+          alert('ok')
+        }
+      })
+    },
+    dragItemsChange: function($el, options){
       let $items = $el.find('.items .item');
 
       let $from;
@@ -280,13 +301,17 @@ urlBase = urlBase.replace('dw-list.js', '');
       let $fromItem;
       let $toItem;
 
-      $items.on({
+      $items.bind({
         dragstart: function(event){
           $from = $(event.target).data('id');
+          $el.find('.item').css({
+            'background': '#fff'
+          })
+          $(event.target).addClass('fromMoved');
           // console.log("from: ", $from);
         },
         dragenter: function(event){
-          $to = $(event.target).parent().data('id');
+          $to = $(event.target).data('id');
           // console.log("to: ", $to);
         },
         dragover: function(event){
@@ -299,6 +324,9 @@ urlBase = urlBase.replace('dw-list.js', '');
         },
         dragend: function(event){
           // console.log('dragend', $(event.target))
+          $(event.target).removeClass('fromMoved');
+        },
+        drop: function(event){
           $fromItem = $el.find('.items .item[data-id="' + $from + '"]');
           $toItem = $el.find('.items .item[data-id="' + $to + '"]');
 
@@ -307,6 +335,20 @@ urlBase = urlBase.replace('dw-list.js', '');
 
           $fromItem.html($toItemHtml);
           $toItem.html($fromItemHtml);
+
+          let $fromItemPriority = $fromItem.find('.position').text();
+          let $toItemPriority = $toItem.find('.position').text();
+
+          $fromItem.find('.position').text($toItemPriority);
+          $toItem.find('.position').text($fromItemPriority);
+
+          $fromItem.data('place', $toItemPriority)
+          $toItem.data('place', $fromItemPriority)
+
+          // prevent different background
+          $el.find('.item').css({
+            'background': '#fff'
+          })
 
         }
       })
